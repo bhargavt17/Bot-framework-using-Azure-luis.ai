44cb4e369310c7300965850eebc36c5767235071
bhargavt17 <bhargavt17@gmail.com>
2018-10-08 10:17:14 +0530

    Add files via upload

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.SimpleEchoBot.sln -p:DeployOnBuild=true -p:PublishProfile=ctsbot123-Web-Deploy.pubxml -p:Password=vTfjZMRjmBJslZAn5BY1b8G4sJLHGPK4FfszrTptdnjJDJaHdYe3d4ma5kMh
+
